6e474d7a00ed61f9de4ecb0021dff85eba2c575f
Libby Lebyane <lebyane.lm@gmail.com>
2022-07-21 17:52:00 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="random_utilites",
+    version="1.0.1",
+    author="Hetchfund.Capital (Libby Lebyane)",
+    author_email="<lebyane.lm@gmail.com>",
+    description="Helper library for random operations",
+    # package_dir={"": "random_utilities"},
+    packages=find_packages(),
+    install_requires=["console", "flask"],
+    keywords=["random", "utilities", "tools", "helpers"]
+)
